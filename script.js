908c0d03ddf426ba55d530adab20ca489e319ed0
Salvame1 <114578756+Salvame1@users.noreply.github.com>
2022-12-02 14:33:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+
+
+const change = document.getElementById("snow");
+change. addEventListener("click", (event) => {
+    event.target.style.background = "green"
+    event.target.style.color = "black";
+}); 
+
+const change2 = document.getElementById("water");
+change2. addEventListener("click", (event) => {
+    event.target.style.background = "navy"
+    event.target.style.color = "white";
+});
+
+const change3 = document.getElementById("house in forest");
+change3. addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "white";
+});
+const change4 = document.getElementById("human and nature");
+change4. addEventListener("click", (event) => {
+    event.target.style.background = "blue"
+    event.target.style.color = "black";
+});
+const change5 = document.getElementById("natural sight 2");
+change5. addEventListener("click", (event) => {
+    event.target.style.background = "brown"
+    event.target.style.color = "white";
+});
+const change6 = document.getElementById("nature and birds");
+change6. addEventListener("click", (event) => {
+    event.target.style.background = "purple"
+    event.target.style.color = "yellow";
+});
+const change7 = document.getElementById("green");
+change7. addEventListener("click", (event) => {
+    event.target.style.background = "orange"
+    event.target.style.color = "black";
+});
+const change8 = document.getElementById("nature sight 1");
+change8. addEventListener("click", (event) => {
+    event.target.style.background = "gold"
+    event.target.style.color = "black";
+});
+const change9 = document.getElementById("summer leaves");
+change9. addEventListener("click", (event) => {
+    event.target.style.background = "black"
+    event.target.style.color = "yellow";
+});
+const change10 = document.getElementById("stream");
+change10. addEventListener("click", (event) => {
+    event.target.style.background = "black"
+    event.target.style.color = "white";
+});
+const change11 = document.getElementById("stones");
+change11. addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "green";
+});
+const change12 = document.getElementById("summer");
+change12. addEventListener("click", (event) => {
+    event.target.style.background = "yellow"
+    event.target.style.color = "purple";
+});
